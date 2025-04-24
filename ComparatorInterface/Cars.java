627002062952c24d93a0922517090613853d2fb4
prabhatchauhan04 <prabhat14chauhan2004@gmail.com>
2025-04-24 14:10:55 +0530

    Comparator Interface

---
+++ ComparatorInterface/Cars.java
@@ -0,0 +1,26 @@
+package ComparatorInterface;
+
+public class Cars{
+    int price;
+    int speed;
+    String color;
+
+    public Cars(){
+
+    }
+
+    public Cars(int price , int speed , String color){
+        this.price = price;
+        this.speed = speed;
+        this.color = color;
+    }
+
+    @Override
+    public String toString(){
+        return "P: " + this.price + "   S: " + this.speed + "  C: " + this.color;
+    }
+
+}
+
+
+
---
+++ ComparatorInterface/Cars_Client.java
@@ -0,0 +1,39 @@
+package ComparatorInterface;
+
+import java.util.*;
+
+public class Cars_Client {
+    public static void main(String[] args) {
+        Cars[] arr = new Cars[5];
+        arr[0] = new Cars(200, 10, "White");
+		arr[1] = new Cars(1000, 20, "Black");
+		arr[2] = new Cars(345, 3, "Yellow");
+		arr[3] = new Cars(34, 89, "Grey");
+		arr[4] = new Cars(8907, 6, "Red");
+        Arrays.sort(arr , new Comparator<Cars>() {
+            public int compare(Cars o1 , Cars o2){
+                return o1.price-o2.price;
+            }
+        });
+        Display(arr);
+        System.out.println();
+        Arrays.sort(arr , new Comparator<Cars>() {
+            @Override
+            public int compare(Cars o1 , Cars o2){
+                return o2.speed-o1.speed;
+            }
+        });
+        Display(arr);
+    }
+    public static void Display(Cars[] arr){
+        for(int i = 0 ; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+
+
+
+
+
