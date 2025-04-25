36ebcfeb946ba7959a6da37d7c4a6915d37e9ab3
prabhatchauhan04 <prabhat14chauhan2004@gmail.com>
2025-04-25 22:37:37 +0530

    HashMap Implementation using arraylist

--- HashMapImplementation/HashMap.java
+++ HashMapImplementation/HashMap.java
@@ -9,6 +9,7 @@ public class HashMap<K , V> {
         Node next;
     }
     private ArrayList<Node> bukt;
+    int size = 0;
     public HashMap(int n){
         bukt = new ArrayList<>();
         for (int i = 0; i < n; i++) {
@@ -18,4 +19,121 @@ public class HashMap<K , V> {
     public HashMap(){
         this(4); // same as HashMap(4) 
     }
+    public void put(K key , V value){
+        int idx = Hashfun(key);
+        Node temp = bukt.get(idx);
+        while (temp != null) {
+            if (temp.key.equals(key)) {
+                temp.value = value;
+                return;
+            }
+            temp = temp.next;
+        }
+        Node nn = new Node();
+        nn.value = value;
+        nn.key = key;
+        temp = bukt.get(idx);
+        nn.next = temp;
+        bukt.set(idx , nn); // update krdi value
+        size++;
+        // (1.0 * size) krdiya taki size double mein convert hojae
+        double lf = (1.0 * size) / bukt.size(); // load factor
+		double thf = 2.0; // threshold factor
+		if (lf > thf) {
+			rehashing();
+		}
+    }
+    private void rehashing(){
+        ArrayList<Node> a = new ArrayList<>();
+		for (int i = 0; i < 2 * bukt.size(); i++) {
+			a.add(null);
+		}
+		ArrayList<Node> old_bukt = bukt;
+		bukt = a;
+		size = 0; // bcoz ab size "put" method apne aap badhata rahega for new bucket 
+		for (Node temp : old_bukt) {
+			while (temp != null) {
+				put(temp.key, temp.value);
+				temp = temp.next;
+			}
+		}
+    }
+    public boolean containsKey(K key){
+        int idx = Hashfun(key);
+        Node temp = bukt.get(idx);
+        while (temp != null) {
+            if (temp.key.equals(key)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+    public V get(K key){
+        int idx = Hashfun(key);
+        Node temp = bukt.get(idx);
+        while (temp != null) {
+            if (temp.key.equals(key)) {
+                return temp.value;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+    public V remove(K key) {
+		int idx = Hashfun(key);
+		Node curr = bukt.get(idx);
+		Node prev = null;
+		while (curr != null) {
+			if (curr.key.equals(key)) {
+				break;
+			}
+			prev = curr;
+			curr = curr.next;
+		}
+        if(curr == null){
+            //jo element remove krna tha woh present hi nhi hai map mein
+            return null;
+        }else if(prev == null){
+            //remove first of linkedlist
+            bukt.set(idx , curr.next);
+            curr.next = null;
+            size--;
+            return curr.value;
+        }else{
+            // remove last ya beech ka element 
+            prev.next = curr.next;
+            curr.next = null;
+            size--;
+            return curr.value;
+        }
+    }
+    public int Hashfun(K key){
+        int n = key.hashCode() % bukt.size();
+        if (n < 0) {
+            n += bukt.size();
+        }
+        return n;
+    }
+    public String toString() {
+		String s = "";
+		for (Node temp : bukt) {
+			while (temp != null) {
+				s = s + temp.key + "=" + temp.value + "  ";
+				temp = temp.next;
+			}
+		}
+		return s;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
--- HashMapImplementation/HashMap_Client.java
+++ HashMapImplementation/HashMap_Client.java
@@ -2,6 +2,18 @@ package HashMapImplementation;
 
 public class HashMap_Client {
     public static void main(String[] args) {
-        
+        HashMap<String , Integer> map = new HashMap<>();
+        map.put("Raj", 89);
+		map.put("Ankit", 79);
+		map.put("Kaju", 99);
+		map.put("Ankita", 59);
+		map.put("Vishal", 77);
+		map.put("Pooja", 88);
+		map.put("Anku", 88);
+		map.put("Puneet", 73);
+		map.put("Ankita", 38);
+		System.out.println(map.get("Ankita"));
+		System.out.println(map.get("Ankit"));
+		System.out.println(map);
     }
 }
