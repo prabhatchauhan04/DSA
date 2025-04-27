f63051613dbbf1805b2d050b85d012dcb4ddb255
prabhatchauhan04 <prabhat14chauhan2004@gmail.com>
2025-04-27 18:42:07 +0530

    Valid Tree LeetCode

--- Graphs/GraphValidTree.java
+++ Graphs/GraphValidTree.java
@@ -1,5 +1,5 @@
 package Graphs;
-
+import java.util.*;
 /*
  LeetCode Question 261. Graph Valid Tree
  Given n nodes labeled from 0 to n-1 and a list of undirected 
@@ -18,5 +18,51 @@ undirected, [0,1] is the same as [1,0] and thus will not appear together in edge
  */
 
 public class GraphValidTree {
-    
+    class Solution {
+		public boolean validTree(int n, int[][] edges) {
+            // ye bnadiya apne type ka graph input se
+            HashMap<Integer , List<Integer>> map = new HashMap<>();
+            for (int i = 0 ; i < n ; i++) {
+                map.put(i, new ArrayList<>());
+            }
+            for (int i = 0; i < edges.length; i++) {
+                int v1 = edges[i][0];
+                int v2 = edges[i][1];
+                map.get(v1).add(v2);
+                map.get(v2).add(v1);
+            }
+            // Checking for valid tree
+            Stack<Integer> st = new Stack<>();
+            HashSet<Integer> visited = new HashSet<>();
+            // Number of Components in Graph
+            int components = 0;
+            // DFT
+            for (int key : map.keySet()) {
+                if (visited.contains(key)) {
+                    continue;
+                }
+                components++;
+                st.push(key);
+                // DFS
+                while (st.isEmpty() == false) {
+                    int rv = st.pop();
+                    if (visited.contains(rv)) {
+                        // cycle detect krega ye block
+                        return false;
+                    }
+                    visited.add(rv);
+                    for (int nbrs : map.get(rv)) {
+                        if (!visited.contains(nbrs)) {
+                            st.push(nbrs);
+                        }
+                    }
+                }
+            }
+            // true return krdega agar components sirf ek hi hai graph mein
+            return components==1;
+        }
+    }
 }
+
+
+
