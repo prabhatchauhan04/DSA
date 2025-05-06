c55f88713d38a06975148928040dbb0a93de3a3f
prabhatchauhan04 <prabhat14chauhan2004@gmail.com>
2025-05-06 17:20:43 +0530

    Fib using topdown & bottomup

--- DP/Fibonacci.java
+++ DP/Fibonacci.java
@@ -5,9 +5,11 @@ public class Fibonacci {
         int n = 6;
         // n+1 size bcoz hum fib(n) humare nth index pr store krenge .
         int[] dp = new int[n+1];
-        System.out.println(fibo(n , dp));;
+        System.out.println(fiboTD(n , dp));
+        System.out.println(fiboBU(n));
     }
-    public static int fibo(int n , int[] dp){
+    // Top Down
+    public static int fiboTD(int n , int[] dp){
         if (n == 0 || n == 1) {
             return n;
         }
@@ -15,8 +17,18 @@ public class Fibonacci {
         if (dp[n] != 0) {
             return dp[n];
         }
-        int f1 = fibo(n-1 , dp);
-        int f2 = fibo(n-2 , dp);
+        int f1 = fiboTD(n-1 , dp);
+        int f2 = fiboTD(n-2 , dp);
         return dp[n] = f2+f1; // values yaad kra rhe in dp array
+    }
+    // Bottom Up
+    public static int fiboBU(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2 ; i < dp.length ; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
