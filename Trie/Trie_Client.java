2c9d8c2d4d50f39330d43e6ebd07fd32ac28ec96
prabhatchauhan04 <prabhat14chauhan2004@gmail.com>
2025-05-13 04:58:02 +0530

    Trie

---
+++ Trie/Trie_Client.java
@@ -0,0 +1,17 @@
+package Trie;
+
+public class Trie_Client {
+    public static void main(String[] args) {
+		Trie t = new Trie();
+		t.insert("apple");
+		t.insert("raj");
+		t.insert("rajesh");
+		t.insert("ankit");
+		t.insert("ankita");
+		t.insert("vishal");
+		t.insert("kaju");
+		t.insert("bitu");
+	}
+}
+
+
